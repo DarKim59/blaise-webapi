4998c9f6a7c850e68ac2d89ae3b2cd35b3b72fa8
jamie kerr <jamiestephenkerr@gmail.com>
2020-07-02 16:11:28 +0100

    Added controller methids

--- Blaise.Api/Controllers/SurveyController.cs
+++ Blaise.Api/Controllers/SurveyController.cs
@@ -27,11 +27,44 @@ namespace Blaise.Api.Controllers
             return Ok(surveys);
         }
 
+        [HttpGet]
+        [Route("parks/{parkName}/surveys")]
+        [ResponseType(typeof(IEnumerable<ISurvey>))]
+        public IHttpActionResult GetSurveysByPark([FromUri] string parkName)
+        {
+            var surveys = _blaiseApi
+                .WithServerPark(parkName)
+                .Surveys;
+
+            return Ok(surveys);
+        }
+
         [HttpPost]
         [Route("surveys/backup")]
         public IHttpActionResult BackupSurveys([FromBody] BackupModel backupModel)
         {
             var surveys = _blaiseApi.Surveys;
+
+            foreach (var survey in surveys)
+            {
+                _blaiseApi
+                    .WithServerPark(survey.ServerPark)
+                    .WithInstrument(survey.Name)
+                    .Survey
+                    .ToPath(backupModel.DestinationPath)
+                    .Backup();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("parks/{parkName}/surveys/backup")]
+        public IHttpActionResult BackupSurveysByPark([FromUri] string parkName, [FromBody] BackupModel backupModel)
+        {
+            var surveys = _blaiseApi
+                .WithServerPark(parkName)
+                .Surveys;
 
             foreach (var survey in surveys)
             {
